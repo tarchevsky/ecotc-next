ea2376aee8883e4dc54582d6ac308c556179b2d1
tarchevsky <i.tarchevsky@gmai.com>
2024-04-24 09:49:49 +0300

    Изменение основной локали за счет localeDetection: false в next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const nextConfig = {
 	reactStrictMode: true,
 	i18n: {
 		locales: ['en', 'ru'],
-		defaultLocale: 'ru'
+		defaultLocale: 'ru',
+		localeDetection: false // Отключает автоматическое определение локали
 	}
 }
 
